5ef9d19592f824c4228f868f4d670fd407ce379b
Jonas Fischer <fijo.com@googlemail.com>
2021-10-17 22:11:35 +0200

    fixed publish action

---
+++ IUxBindingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine.UIElements;
+
+namespace EcsRx.Plugins.UnityUx {
+    public interface IUxBindingService {
+        void PopulateChild(UxContext context, VisualElement container, IUxComponent component);
+        void PopulateChild(UxContext context, VisualElement container, IObservable<IUxComponent> componentObservable);
+        void PopulateChild<T>(UxContext context, VisualElement container, IObservable<T> modelObservable) where T : IUxComponent;
+        void PopulateChildren(UxContext context, VisualElement container, IEnumerable<IUxComponent> models);
+        void PopulateChildren<T>(UxContext context, VisualElement container, IEnumerable<T> models) where T : IUxComponent;
+        void PopulateChildren(UxContext context, VisualElement container, IReadOnlyReactiveCollection<IUxComponent> components);
+        void PopulateChildren<T>(UxContext context, VisualElement container, IReadOnlyReactiveCollection<T> components) where T : IUxComponent;
+    }
+}
---
+++ UxContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Serilog;
+using UniRx;
+
+namespace EcsRx.Plugins.UnityUx {
+    public sealed class UxContext {
+        public UxContext Parent { get; }
+        public IUxComponent Component { get; }
+        public IObservable<Unit> Destroy { get; }
+        public ILogger Logger { get; } // TODO figure out a way of doing logging without introducing a serilog dependency
+
+        public UxContext(UxContext parent, IUxComponent component, IObservable<Unit> destroy, ILogger logger) {
+            Parent = parent;
+            Component = component;
+            Destroy = destroy ?? throw new ArgumentNullException(nameof(destroy));
+            Logger = logger ?? throw new ArgumentNullException(nameof(destroy));
+        }
+
+        public UxContext TakeUntil(IObservable<Unit> destroy) {
+            if (destroy is null)
+                throw new ArgumentNullException(nameof(destroy));
+            return new UxContext(this, null, Destroy.Merge(destroy), Logger);
+        }
+
+        public T GetAncestor<T>() where T : IUxComponent {
+            var current = this;
+            do {
+                current = current.Parent;
+                if (current == null)
+                    throw new ArgumentException($"No ancestor found using the component type '{typeof(T)}'", nameof(T));
+            } while (!(current.Component is T));
+            return (T)current.Component;
+        }
+
+        public static UxContext CreateRootContext(IObservable<Unit> destroy, ILogger logger) {
+            return new UxContext(null, null, destroy, logger);
+        }
+    }
+}
