49af052394fdb848e9872f918e31d5c8d3e32b54
Jonas Fischer <fijo.com@googlemail.com>
2021-11-20 14:44:41 +0100

    added VisualElement.ToUxComponent() extension and Binder for that

---
+++ UnityUxPlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using SystemsRx.Executor.Handlers;
+using SystemsRx.Infrastructure.Dependencies;
+using SystemsRx.Infrastructure.Plugins;
+using SystemsRx.Infrastructure.Extensions;
+using SystemsRx.Systems;
+
+namespace EcsRx.Plugins.UnityUx {
+    public sealed class UnityUxPlugin : ISystemsRxPlugin {
+        public string Name => "Unity Ux Plugin";
+        public Version Version { get; } = new Version("2.0.2");
+
+        public void SetupDependencies(IDependencyContainer container) {
+            container.Bind<IUxBindingStore, UxBindingStore>(x => x.AsSingleton());
+            container.Bind<IUxBinderProvider>(x => x.ToMethod(c => c.Resolve<IUxBindingStore>()));
+            container.Bind<IConventionalSystemHandler, UxBindingRegistrationHandler>();
+            container.Bind<IUxBindingService, UxBindingService>(x => x.AsSingleton());
+            container.Bind<ISystem, StaticComponentBinder>(x => x.AsSingleton());
+        }
+
+        public IEnumerable<ISystem> GetSystemsForRegistration(IDependencyContainer container) => new ISystem[0];
+    }
+}
---
+++ UxBindingStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using SystemsRx.Systems;
+
+namespace EcsRx.Plugins.UnityUx {
+    sealed class UxBindingStore : IUxBindingStore {
+        public IDictionary<Type, ISystem> BinderByComponentType { get; } = new Dictionary<Type, ISystem>();
+
+        public IUxBinder<T> GetBinder<T>() where T : IUxComponent {
+            var componentType = typeof(T);
+            if (BinderByComponentType.TryGetValue(componentType, out var system))
+                return (IUxBinder<T>)system;
+
+            throw new ArgumentException($"No binder was registered to handle component type '{typeof(T)}'.", nameof(T));
+        }
+    }
+}
